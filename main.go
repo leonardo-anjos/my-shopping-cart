df1375c259cefcf955d760f1a2bf8be16bf8a463
Leonardo Anjos <leodrums11@gmail.com>
2024-03-10 19:57:47 -0300

    main

--- main.go
+++ main.go
@@ -9,27 +9,28 @@ import (
 	"github.com/leonardo-anjos/my-shopping-cart/controllers"
 	"github.com/leonardo-anjos/my-shopping-cart/database"
 	"github.com/leonardo-anjos/my-shopping-cart/middleware"
+	"github.com/leonardo-anjos/my-shopping-cart/routes"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
-
 	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
-	router.ApplicationRoutes(router)
+	routes.UserRoutes(router)
 	router.Use(middleware.Authentication())
-
-	router.Get("/addtocart", app.AddToCart())
-	router.Get("/removeitem", app.RemoveItem())
-	router.Get("/cartchackout", app.BuyFromCart())
-	router.Get("/instantbuy", app.InstantBuy())
-
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/listcart", controllers.GetItemFromCart())
+	router.POST("/addaddress", controllers.AddAddress())
+	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
+	router.PUT("/editworkaddress", controllers.EditWorkAddress())
+	router.GET("/deleteaddresses", controllers.DeleteAddress())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
 	log.Fatal(router.Run(":" + port))
 }
