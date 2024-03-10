471a34d581a0d585640f788a55df06869c0c0231
Leonardo Anjos <leodrums11@gmail.com>
2024-03-10 19:23:57 -0300

    refactor database config

--- database/databaseSetup.go
+++ database/databaseSetup.go
@@ -8,41 +8,38 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"golang.org/x/net/context"
 )
 
-func dbSet() *mongo.Client {
+func DBSet() *mongo.Client {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
 	defer cancel()
-
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	client.Ping(context.TODO(), nil)
+	err = client.Ping(context.TODO(), nil)
 	if err != nil {
-		log.Println("failed to connect to mongodb :(")
+		log.Println("failed to connect to mongodb")
 		return nil
 	}
-
-	fmt.Println("sucessfully connected to mongodb :)")
+	fmt.Println("Successfully Connected to the mongodb")
 	return client
 }
 
-var Client *mongo.Client = dbSet()
+var Client *mongo.Client = DBSet()
 
-func userData(client *mongo.Client, collectionName string) *mongo.Collection {
+func UserData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return collection
 
 }
 
-func productData(client *mongo.Client, collectionName string) *mongo.Collection {
-
+func ProductData(client *mongo.Client, CollectionName string) *mongo.Collection {
+	var productcollection *mongo.Collection = client.Database("Ecommerce").Collection(CollectionName)
+	return productcollection
 }
